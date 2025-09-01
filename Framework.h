e4cff89293dd22951561d44fe1a9f73c6322eaa9
Lunti <feraogfn@gmail.com>
2025-09-01 20:58:18 +1000

    Create Framework.h

---
+++ Framework.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <thread>
+#include <mutex>
+
+// Windows networking
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+// link with Ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
